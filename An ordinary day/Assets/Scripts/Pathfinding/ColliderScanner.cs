ac9bc9cb62ab2ae520a90cd8be901916b9335154
Vincent <vincent.rynik@gmail.com>
2019-07-06 16:14:54 +0200

    Worked on collider scanner

---
+++ An ordinary day/Assets/Scripts/Pathfinding/ColliderScanner.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Scan the presence of colliders in the given area
+/// </summary>
+public class ColliderScanner : MonoBehaviour
+{
+    private const float ScanRadiusDivider = 3f;
+
+    [Header("Scan parameters")]
+    [SerializeField]
+    [Tooltip("Unity distance unit")]
+    private float _spacialResolution = 1f;
+    [SerializeField]
+    private float _refreshRateSec = 1f;
+    [SerializeField]
+    private Rect _scanArea;
+
+    [Header("Layers")]
+    [SerializeField]
+    private LayerMask[] _dynamicLayersToScan;
+    [SerializeField]
+    private LayerMask[] _staticLayersToScan;
+
+    [Header("Debug")]
+    [SerializeField]
+    private bool _showDebug = true;
+    [SerializeField]
+    private Texture2D _areaTexture;
+    [SerializeField]
+    private Color _nOkColor;
+
+    private float _lastRefreshTime;
+    private WorldGrid<bool> _scanResult;
+    private List<Vector2> _pointsToScan; // store in order to optimize computations
+
+
+    #region Init
+    private void InitScanGrid()
+    {
+        _scanResult = new WorldGrid<bool>(_scanArea, _spacialResolution);
+        _pointsToScan = _scanResult.GetWorldCooordinateList();
+    }
+
+
+    private void Start()
+    {
+        InitScanGrid();
+        ScanStaticLayers();
+        ScanDynamicLayers();
+        //Debug.Log("Collider at debug position: " + _scanResult.Get(new Vector2(-9.42f, 1.12f)));
+    }
+    #endregion
+
+
+    private void Update()
+    {
+        if (Time.time >= _lastRefreshTime + _refreshRateSec)
+        {
+            ScanDynamicLayers();
+        }
+    }
+
+
+    #region Scanning
+    private void ScanStaticLayers()
+    {
+        Debug.Log("[ColliderScanner] Scan Static Layer");
+        Scan(_staticLayersToScan);
+        // Remove all the static points that already have colliders
+        var n = _pointsToScan.Count;
+        _pointsToScan.RemoveAll ((point) => _scanResult.Get(point));
+        var nAfterOpti = _pointsToScan.Count;
+        Debug.Log("Scan optimization. Removed " + (n - nAfterOpti) + " points to scan.");
+    }
+
+
+    private void ScanDynamicLayers()
+    {
+        Debug.Log("[ColliderScanner] ScanDynamicLayers");
+        _lastRefreshTime = Time.time;
+        Scan(_dynamicLayersToScan);
+    }
+
+
+    private void Scan(LayerMask[] layers)
+    {
+        if (layers == null || layers.Length == 0)
+            return;
+        foreach (var layer in layers)
+            Scan(layer);
+    }
+
+
+    private void Scan(LayerMask layer)
+    {
+        foreach (var point in _pointsToScan)
+            _scanResult.Set(point, HasCollider(point, layer));
+    }
+
+    #endregion
+
+
+    #region Utils
+    private bool HasCollider(Vector2 point, LayerMask layer)
+    {
+        return Physics2D.OverlapCircle(point, _spacialResolution / ScanRadiusDivider, layer) != null;
+    }
+
+    #endregion
+
+
+    #region Debug
+    private void OnDrawGizmos()
+    {
+        // area
+        if (!_showDebug)
+            return;
+        Gizmos.DrawGUITexture(_scanArea, _areaTexture);
+        // Draw nOk Areas
+        var nOKPoints = GetAllNOkPoints();
+        Gizmos.color = _nOkColor;
+        foreach (var point in nOKPoints)
+            Gizmos.DrawCube(point, Vector3.one * 0.5f);
+                     
+    }
+
+
+    private List<Vector2> GetAllNOkPoints()
+    {
+        var nOKPoints = new List<Vector2>();
+        if (_scanResult != null)
+        {
+            for (int row = 0; row < _scanResult.NRow; row++)
+            {
+                for (int column = 0; column < _scanResult.NColumn; column++)
+                {
+                    if (_scanResult[row, column])
+                        nOKPoints.Add(_scanResult.GetWordCoordinate(row, column));
+                }
+            }
+        }
+        return nOKPoints;
+    }
+    #endregion
+}
---
+++ An ordinary day/Assets/Scripts/Pathfinding/WorldGrid.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+// Generic class to store some information contained in the world map on grid
+public class WorldGrid<T>
+{
+    private T[,] _grid;
+    public int NRow => _grid.GetLength(0);
+    public int NColumn => _grid.GetLength(1);
+
+    // link with world coordinate
+    private Rect _rect;
+    private float _cellSize;
+
+    // Iterator
+    public T this[int x ,int y]
+    {
+        get { return _grid[x,y]; }
+        set { _grid[x,y] = value; }
+    }
+
+    public WorldGrid(Rect rect, float cellSize)
+    {
+        _rect = rect;
+        _cellSize = cellSize;
+        // grid initialization
+        var nRow = (int) Mathf.Ceil(rect.height / cellSize);
+        var nCol = (int) Mathf.Ceil(rect.width / cellSize);
+        _grid = new T[nRow, nCol];
+        Debug.Log("Create World Grid");
+        Debug.Log("N Elements: " + NRow * NColumn);
+    }
+
+
+    public void Set(Vector2 worldPosition, T value)
+    {
+        var index = GetGridIndex(worldPosition);
+        //Debug.Log(string.Format("Set at {0} from world position {1}", string.Join(", ", index), worldPosition.ToString()));
+        _grid[index.x, index.y] = value;
+    }
+
+
+    public T Get(Vector2 worldPosition)
+    {
+        var gridIndex = GetGridIndex(worldPosition);
+        return _grid[gridIndex.x, gridIndex.y];
+    }
+
+
+    public List<Vector2> GetWorldCooordinateList()
+    {
+        var list = new List<Vector2>();
+        for (int row = 0; row < NRow; row++)
+        {
+            for (int column = 0; column < NColumn; column++)
+                list.Add(GetWordCoordinate(row, column));
+        }
+        return list;
+    }
+
+
+    public Vector2 GetWordCoordinate(int row, int column)
+    {
+        var x = _rect.x + column * _cellSize + _cellSize / 2; // middle
+        var y = _rect.y + row * _cellSize + _cellSize / 2; // middle
+        return new Vector2(x, y);
+    }
+
+
+    public Vector2Int GetGridIndex(Vector2 worldPosition)
+    {
+        var row = (int) Mathf.Ceil ((worldPosition.y - _rect.yMin) / _cellSize) - 1;
+        var column = (int) Mathf.Ceil ((worldPosition.x - _rect.xMin) / _cellSize) - 1;
+        return new Vector2Int(row, column);
+    }
+
+
+    public Vector2 GetWordCoordinate(Vector2Int gridIndex) => GetWordCoordinate(gridIndex.x, gridIndex.y);
+}
