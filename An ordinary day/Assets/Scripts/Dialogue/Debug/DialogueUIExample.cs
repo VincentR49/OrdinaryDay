6140506036bc63e1ad18aa4f7d536779726eee03
Vincent <vincent.rynik@gmail.com>
2019-08-20 22:35:13 +0200

    Preparing debug scene of Yarn

---
+++ An ordinary day/Assets/Scripts/Dialogue/Debug/DialogueUIExample.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using Yarn;
+
+public class DialogueUIExample : Yarn.Unity.DialogueUIBehaviour
+{
+    [SerializeField]
+    private Text _text;
+
+    public override IEnumerator RunCommand(Command command)
+    {
+        // todo
+        throw new System.NotImplementedException();
+    }
+
+    public override IEnumerator RunLine(Line line)
+    {
+        // todo do more stuff here
+        _text.text = line.text;
+        yield return null;
+    }
+
+    public override IEnumerator RunOptions(Options optionsCollection, OptionChooser optionChooser)
+    {
+        // todo
+        throw new System.NotImplementedException();
+    }
+}
