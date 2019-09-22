2cb4de191fc251abfcf69311b2de905902b2ecc0
Vincent <vincent.rynik@gmail.com>
2019-09-22 16:19:23 +0200

    Removed screen fader from sceneLoaderMain (debug) and added some comments

--- An ordinary day/Assets/Scripts/Fader/SingletonFactory.cs
+++ An ordinary day/Assets/Scripts/Fader/SingletonFactory.cs
@@ -1,10 +1,15 @@
 ﻿using UnityEngine;
 
+/// <summary>
+/// Use this class to instanciate a prefab based game object which will be persistant among scene.
+/// Usefull for object like screenFader, gameController etc.
+/// Allow to avoid to duplicate those objects in all the game scenes.
+/// </summary>
 public class SingletonFactory : Singleton<SingletonFactory>
 {
+    // TODO find more generic way to write this (can be messy with a lot of prefabs)
     private static GameObject _screenFaderPrefab = Utils.GetPrefab(PathConstants.ScreenFaderPrefab);
     private static GameObject _screenFaderInstance;
-
 
     public static GameObject ScreenFader()
     {
